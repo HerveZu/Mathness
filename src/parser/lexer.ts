c429aa3a9d9b28f732ae5c1ff46fe3439f9ec426
Hervé Zucchinetti <hz@swissmediapartners.ch>
2026-02-11 10:04:56 +0100

    Update function list and improve lexer for case-insensitive constants and functions

--- src/parser/lexer.ts
+++ src/parser/lexer.ts
@@ -3,7 +3,7 @@ import { createLexer } from 'leac';
 export const lexer = createLexer([
   { name: 'ws', regex: /\s+/, discard: true },
 
-  { name: 'function', regex: /log|sin|cos|tan|sqrt|abs/ },
+  { name: 'function', regex: /log|sin|cos|tan|sqrt|abs/i },
   { name: 'variable', str: 'x' },
 
   { name: 'number', regex: /0|([1-9][0-9]?)/ }, // 0 or non-zero int (max 2 digits)
@@ -14,7 +14,7 @@ export const lexer = createLexer([
   { name: 'div', str: '/' },
   { name: 'caret', str: '^' },
 
-  { name: 'const', regex: /pi|e/ },
+  { name: 'const', regex: /pi|e/i },
 
   { name: 'lparen', str: '(' },
   { name: 'rparen', str: ')' },
